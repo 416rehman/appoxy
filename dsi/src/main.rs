b390ea5e23286d71c08ab1a573ea0f7a42b42474
416rehman <15690606+416rehman@users.noreply.github.com>
2022-10-30 20:18:17 -0400

    added streaming endpoint example

--- dsi/src/main.rs
+++ dsi/src/main.rs
@@ -1,4 +1,8 @@
 #[macro_use] extern crate rocket;
+use std::io;
+use std::io::{Error, ErrorKind};
+use rocket::response::stream::ReaderStream;
+use rocket::tokio::io::BufReader;
 
 #[cfg(test)] mod integration_tests;
 mod routers;
@@ -10,7 +14,23 @@ fn index() -> &'static str {
     "Hello, world!"
 }
 
+/// Streams the output of a command as a response.
+/// Use "curl -N" to stream the output.
+#[get("/stream")]
+fn hello() -> io::Result<ReaderStream![BufReader<tokio::process::ChildStdout>]> {
+    let stdout = tokio::process::Command::new("ls")
+        .arg("-la")
+        .stdout(std::process::Stdio::piped())
+        .spawn()?
+        .stdout
+        .take()
+        .ok_or_else(|| Error::new(ErrorKind::Other,"Could not capture standard output."))?;
+
+    let reader = BufReader::new(stdout);
+    Ok(ReaderStream::one(reader))
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index, routers::droids_router::new, routers::stacks_router::common])
+    rocket::build().mount("/", routes![index, routers::droids_router::new, routers::stacks_router::common, sleep, hello])
 }
